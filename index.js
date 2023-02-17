9ffc01dabd26350de89b804355cdcca5d3f5eb94
Landy510 <randy.chen@tpisoftware.com>
2023-02-17 11:55:06 +0800

    fix: wrong element trigger click event 1. fix: Via binding click and moueover event to header-optionLink element the problem of retrieving wrong clicked element of the triggerd click event is solved.

--- index.js
+++ index.js
@@ -1,30 +1,29 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   const optionArr = [...document.querySelectorAll('.header-option')]
   optionArr.forEach(option => {
-    option.addEventListener('mouseover', (evt) => {
-      console.log('enter', evt.currentTarget);
-      evt.stopPropagation();
-      const isActive = [...evt.currentTarget.offsetParent.classList].find(className => className === 'header-option-active')
-      if(!isActive) evt.currentTarget.offsetParent.classList.add('header-option-active');
-    }, false)
-
     option.addEventListener('mouseleave', (evt) => {
       evt.stopPropagation();
-      console.log('leave?')
       if(option !== evt.currentTarget) return;
       const isActive = [...evt.target.classList].find(className => className === 'header-option-active')
       if(isActive) evt.target.classList.remove('header-option-active');
     }, false)
-    
-    option.addEventListener('click', function(evt) {
-      console.log('click', window.event.currentTarget);
-      const hasSubMenu = [...evt.currentTarget.children].find(el => el.className.includes('header-submenu-list'));
-      if(!hasSubMenu) {
-        evt.stopPropagation();
-        return;
-      } // check is the clicked element has submenu element, if no, then there's no need to check for it whether it need to add 'header-option-active' to its class or not.
-      hasSubMenu && evt.stopPropagation();
-      option === evt.currentTarget && evt.currentTarget.classList.toggle('header-option-active');
+  })
+
+  const optionLinkArr = [...document.querySelectorAll('.header-option .header-optionLink')]
+  optionLinkArr.forEach(optionLink => {
+    optionLink.addEventListener('mouseover', (evt) => {
+      const isMobileActive = window.innerWidth <= 961; // While device is in mobile mode, mouseenter event is no need to excute.
+      if(isMobileActive) return;
+      evt.stopPropagation();
+      evt.currentTarget.parentElement.classList.add('header-option-active');
+      console.log(evt.currentTarget.parentElement.classList)
+    }, false)
+
+    optionLink.addEventListener('click', function(evt) {
+      evt.stopPropagation();
+      const hasSubMenu = [...this.parentElement.children].find(el => el.className.includes('header-submenu-list'));
+      if(!hasSubMenu) return;
+      optionLink === evt.currentTarget && evt.currentTarget.parentElement.classList.toggle('header-option-active');
     }, false)
   })
 
